b91c1d605b5d109c1834a1b6b9cf5d4eb1fafdd7
M Akhyar Rahman Hidayat <akhyarrh@gmail.com>
2020-03-09 19:14:42 +0800

    Remove inline comments Maybe change to block comment will be better

--- sw.js
+++ sw.js
@@ -6,24 +6,19 @@ var urlsToCache = [];
 
 var CACHE_NAME = 'akhyarrh-cache-v1';
 
-// Cache posts
 {% for post in site.posts %}
 urlsToCache.push("{{ post.url }}")
 {% endfor %}
 
-// Cache pages
 {% for page in site.pages %}
 urlsToCache.push("{{ page.url }}")
 {% endfor %}
 
-// Cache assets
 {% for file in site.static_files %}
 urlsToCache.push("{{ file.path }}")
 {% endfor %}
 
-// Installation of service worker
 self.addEventListener('install', function(event) {
-	// Perform install steps
 	event.waitUntil(
 		caches.open(CACHE_NAME)
 		.then(function(cache) {
