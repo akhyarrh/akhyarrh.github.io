e41f9d5334126b0aff3dea89631f2b60d3f17281
M Akhyar Rahman H <akhyarrh@gmail.com>
2020-10-21 07:59:36 +0800

    Update sw.js
    Update cache name version, remove year from it
    Dont cache 404 page

--- sw.js
+++ sw.js
@@ -4,14 +4,16 @@ layout: none
 
 var urlsToCache = [];
 
-var CACHE_NAME = 'akhyarrh-v{{ site.time | date: "%Y%s" }}';
+var CACHE_NAME = 'akhyarrh-v{{ site.time | date: "%s" }}';
 
 {% for post in site.posts %}
 urlsToCache.push("{{ post.url }}")
 {% endfor %}
 
 {% for page in site.pages %}
+{% unless page.url == '/404.html' %}
 urlsToCache.push("{{ page.url }}")
+{% endunless %}
 {% endfor %}
 
 {% for file in site.static_files %}
