6280c0f4d75ff7dd6562810fd6ffa4289d6af3a5
M. Akhyar Rahman H <akhyarrh@users.noreply.github.com>
2020-11-07 01:49:51 +0800

    Rename sw.js to service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 ---
 layout: none
+sitemap: false
 ---
 
 var urlsToCache = [];
@@ -12,14 +13,25 @@ urlsToCache.push("{{ post.url }}")
 
 {% for page in site.pages %}
 {% if page.url == '/404.html' %}
-{% elsif page.url == '/sw.js' %}
+{% elsif page.url == '/service-worker.js' %}
+{% elsif page.url == '/robots.txt' %}
+{% elsif page.url == '/assets/css/main.css.map' %}
+{% elsif page.url == '/assets/js/lunr/lunr-gr.js' %}
+{% elsif page.url contains 'webmanifest' %}
+{% elsif page.url contains 'xml' %}
 {% else %}
 urlsToCache.push("{{ page.url }}")
 {% endif %}
 {% endfor %}
 
 {% for file in site.static_files %}
+{% if file.path == '/assets/js/_main.js' %}
+{% elsif file.path == '/assets/js/lunr/lunr.js' %}
+{% elsif file.path contains '/assets/js/plugins' %}
+{% elsif file.path contains '/assets/js/vendor' %}
+{% else %}
 urlsToCache.push("{{ file.path }}")
+{% endif %}
 {% endfor %}
 
 self.addEventListener('install', function(event) {
