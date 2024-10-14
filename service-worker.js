4739ac271c8c4294cbf26ebb30c0ced64f5fe6c5
M. Akhyar Rahman H. <akhyarrh@users.noreply.github.com>
2024-10-14 14:44:31 +0800

    Change service-worker.js from using workbox to vanilla. Pasted from upstream pixyll sw.js

--- service-worker.js
+++ service-worker.js
@@ -1,88 +1,30 @@
-// set names for both precache & runtime cache
-workbox.core.setCacheNameDetails({
-    prefix: 'akhyarrh',
-    suffix: 'v2',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+---
+layout: null
+---
+var CACHE_NAME = "apropixyllv1";
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        "{{ '/css/style.css' | relative_url }}?{{ site.time | date: '%Y%m%d%H%M' }}",
+        "{{ '/' | relative_url }}"
+      ]);
+    })
+  );
 });
 
-// let Service Worker take control of pages ASAP
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
-
-// let Workbox handle our precache list
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
-workbox.routing.registerRoute(
-  /\.html$/,
-  new workbox.strategies.NetworkFirst({
-    plugins: [
-      new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [200]
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  /\.js$/,
-  new workbox.strategies.CacheFirst()
-);
-
-// search index
-workbox.routing.registerRoute(
-  '/assets/js/lunr/lunr-store.js',
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-  /\.css$/,
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-  /\.(?:jpg|jpeg|png|svg|webp)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [200]
-      }),
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 10, // Only cache 10 images ( TODO: is everything on precache already are counted ?)
-        maxAgeSeconds: 60 * 60 * 24 * 90 // expired after 90 Days
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  /^https?:\/\/cdn.statically.io/,
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-  /^https?:\/\/source.unsplash.com/,
-  new workbox.strategies.StaleWhileRevalidate()
-);
-workbox.routing.registerRoute(
-  /^https?:\/\/images.unsplash.com/,
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-  /^https?:\/\/github.githubassets.com/,
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-  /^https?:\/\/cdn.jsdelivr.net/,
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-  /^https?:\/\/robohash.org/,
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.precaching.cleanupOutdatedCaches();
+self.addEventListener("activate", function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.map(function(name) {
+          if (name != CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  return clients.claim();
+});
