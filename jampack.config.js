78d10fa2d1ec60c7b44578e8c3067490759a70cd
Akhyar <akhyarrh@users.noreply.github.com>
2024-10-21 21:26:47 +0000

    Update jampack.config.js

--- jampack.config.js
+++ jampack.config.js
@@ -1,4 +1,17 @@
 export default {
+  html: {
+    minifyJS: true,
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    removeComments: true,
+    removeEmptyAttributes: true,
+    removeEmptyElements: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    processScripts: ['application/ld+json']
+  },
   image: {
     external: {
       process: 'download',
