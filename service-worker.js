946a479cb60eb4286edb99b06b3a61ec2a134d8e
Muhammad Akhyar Rahman Hidayat <akhyarrh@users.noreply.github.com>
2026-01-15 04:55:19 +0800

    Update CACHE_NAME and cache assets in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
 ---
 layout: null
 ---
-var CACHE_NAME = "apropixyllv1";
+var CACHE_NAME = "akhyarrh-minima-v1";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "{{ '/css/style.css' | relative_url }}?{{ site.time | date: '%Y%m%d%H%M' }}",
-        "{{ '/' | relative_url }}"
+        "{{ '/assets/css/style.css' | relative_url }}?{{ site.time | date: '%Y%m%d%H%M' }}",
+        "{{ '/' | relative_url }}?{{ site.time | date: '%Y%m%d%H%M' }}"
       ]);
     })
   );
