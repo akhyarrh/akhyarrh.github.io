967027a1abdc9bb4dec5e6d67665ed018d7cc81a
Akhyar RH <akhyarrh@users.noreply.github.com>
2026-01-21 15:13:09 +0800

    Update _middleware.tsx

--- functions/_middleware.tsx
+++ functions/_middleware.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import vercelOGPagesPlugin from "@cloudflare/pages-plugin-vercel-og";
 
 export const onRequest = vercelOGPagesPlugin({
-  imagePathSuffix: "/social-image.png",
+  // Gunakan nama file saja untuk menghindari double slash //
+  imagePathSuffix: "social-image.png",
+  
   component: ({ title, siteTitle }) => (
     <div
       style={{
@@ -10,75 +12,32 @@ export const onRequest = vercelOGPagesPlugin({
         flexDirection: "column",
         width: "100%",
         height: "100%",
-        backgroundColor: "#fdfdfd", // --minima-background-color
+        backgroundColor: "#fdfdfd",
         padding: "80px",
         justifyContent: "center",
         fontFamily: "sans-serif",
       }}
     >
-      {/* Garis aksen atas mirip border di Minima */}
-      <div 
-        style={{ 
-          display: "flex",
-          position: "absolute", 
-          top: 0, 
-          left: 0, 
-          right: 0, 
-          height: "8px", 
-          backgroundColor: "#e7e7e7" // --minima-border-color-01
-        }} 
-      />
-
-      {/* Judul Situs (Site Title) */}
-      <div
-        style={{
-          fontSize: "24px",
-          color: "#818181", // --minima-brand-color
-          marginBottom: "20px",
-          fontWeight: "normal",
-        }}
-      >
-        {siteTitle || "My Jekyll Blog"}
+      <div style={{ display: "flex", position: "absolute", top: 0, left: 0, right: 0, height: "8px", backgroundColor: "#e7e7e7" }} />
+      <div style={{ fontSize: "24px", color: "#818181", marginBottom: "20px" }}>
+        {siteTitle || "Blog"}
       </div>
-
-      {/* Judul Postingan (Heading) */}
-      <div
-        style={{
-          display: "flex",
-          fontSize: "65px",
-          fontWeight: "bold",
-          color: "#111", // --minima-heading-color
-          lineHeight: 1.2,
-          letterSpacing: "-0.02em",
-        }}
-      >
-        {title}
+      <div style={{ display: "flex", fontSize: "65px", fontWeight: "bold", color: "#111", lineHeight: 1.2 }}>
+        {title || "Untitled Post"}
       </div>
-
-      {/* Link simulasi atau aksen bawah */}
-      <div
-        style={{
-          display: "flex",
-          marginTop: "40px",
-          fontSize: "20px",
-          color: "#1e69d8", // --minima-link-base-color
-          borderBottom: "1px solid #1e69d8",
-          width: "fit-content",
-        }}
-      >
-        read more
+      <div style={{ display: "flex", marginTop: "40px", fontSize: "20px", color: "#1e69d8" }}>
+        read more...
       </div>
     </div>
   ),
+  
   extractors: {
     on: {
-      // Mengambil Judul Postingan
       "meta[property='og:title']": (element) => ({
-        title: element.getAttribute("content"),
+        title: element.getAttribute("content")
       }),
-      // Mengambil Nama Situs (biasanya ada di og:site_name dari jekyll-seo-tag)
       "meta[property='og:site_name']": (element) => ({
-        siteTitle: element.getAttribute("content"),
+        siteTitle: element.getAttribute("content")
       }),
     },
   },
