a6fa9d7f291f7f08603e284a7e8b8b99bbb17e59
Akhyar RH <akhyarrh@users.noreply.github.com>
2026-02-07 11:24:25 +0800

    Update minify-html.js

--- minify-html.js
+++ minify-html.js
@@ -6,9 +6,10 @@ const minify = require('@minify-html/node');
 const BUILD_DIR = '_site'; 
 
 const cfg = {
+    // https://github.com/wilsonzlin/minify-html/minify-html-nodejs/src/lib.rs#L22-L36
     keep_closing_tags: true,
     keep_html_and_head_opening_tags: true,
-    keep_spaces_between_attributes: true
+    keep_input_type_text_attr: true
 };
 
 function walkDir(dir, callback) {
